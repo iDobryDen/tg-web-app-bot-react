296bfa5d93ff0c88aef2f30ef6fa8109910b004b
iDobryDen <59179395+iDobryDen@users.noreply.github.com>
2024-06-21 23:33:44 +0400

    add route for index and form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import React, { useEffect } from 'react';
 import { useTelegram } from "./hooks/useTelegram";
 import Header from './components/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import ProductList from './components/ProductList/ProductList';
+import Form from './components/Form/Form';
 
 function App() {
 
@@ -12,13 +15,15 @@ function App() {
         tg.ready();
     }, [])
 
-  return (
-    <div className="App">
-        <Header />
-        <b>work</b>
-        <button onClick={onToggleButton}>toggle</button>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Routes>
+                <Route index element={<ProductList />}/>
+                <Route path={'form'} element={<Form />}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
