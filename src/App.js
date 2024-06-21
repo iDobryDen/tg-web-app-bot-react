13fe383bed7d932ff7d6efed749105a158098252
iDobryDen <59179395+iDobryDen@users.noreply.github.com>
2024-06-21 17:12:55 +0400

    pered zalivkoi na git

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+//import { useCookie } from 'nuxt/app';
 import './App.css';
+import React, { useEffect } from 'react';
+
+
+const tg = window.Telegram.WebApp;
 
 function App() {
+
+    useEffect( () => {
+        tg.ready();
+    }, [])
+
+    const onClose = () => {
+        tg.close()
+    }
+
   return (
     <div className="App">
         work
+        <button onClick={onClose}>Закрыть</button>
     </div>
   );
 }
